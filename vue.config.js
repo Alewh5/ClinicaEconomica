c325d6e23f1c8bc52e714e9bec872fc1022bf521
Alewh5 <walejandroh95@gmail.com>
2024-11-19 14:26:54 -0600

    cambiando ruta

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
         devServer: {
             proxy: {
                 '/api': {
-                target: 'http://127.0.0.1:8000',  // URL de tu backend
+                target: 'http://farmaciaesperanza.top',  // URL de tu backend
                 changeOrigin: true,               // Cambiar el origen de la solicitud
                 secure: false,                    // Desactivar SSL (si es necesario)
                 logLevel: 'debug',                // Esto es útil para depurar problemas
